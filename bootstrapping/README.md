f6a7c3ea51f9c78cac06d25e846845dae9a6cba8
Tianyuan Yu <royu9710@outlook.com>
2018-07-20 17:36:52 -0700

    add security bootstrapping

---
+++ bootstrapping/README.md
@@ -0,0 +1,1 @@
+Currently, we can't generate micro-ecc key on samr21-xpro. So we copy the bootstrap key as the communication key in our test. Also, trust anchor' key is the same as bootstrap key in test file.
---
+++ bootstrapping/main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2018 Tianyuan Yu
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdio.h>
+
+extern void ndn_test(void);
+//extern void ndn_bootstrap(void);
+
+int main(void)
+{
+    ndn_test();
+    //ndn_bootstrap();
+
+    /* should be never reached */
+    return 0;
+}
