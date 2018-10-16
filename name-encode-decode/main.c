d49bad9d35f50adc5d7c537f9c2bbc9f52a7138f
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-10-15 22:54:05 -0700

    finish name encode decode

--- name-encode-decode/main.c
+++ name-encode-decode/main.c
@@ -12,56 +12,104 @@
 #include "shell.h"
 #include "msg.h"
 
-static const shell_command_t shell_commands[] = {
-    { NULL, NULL, NULL }
-};
+// static const shell_command_t shell_commands[] = {
+//     { NULL, NULL, NULL }
+// };
 
 int main(void)
 {
     /* start shell */
     puts("All up, running the shell now");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    // char line_buf[SHELL_DEFAULT_BUFSIZE];
 
-    // initialization
-    char name[] = "aaaaaaaaaaaaaaaa";
-    name_component_t component = name_component_from_string(name, sizeof(name));
-    printf("check type %d\n", component.type);
-    printf("check length %zu\n", component.size);
+    // tests start
+
+    // component initialization
+    char comp1[] = "aaaaaa";
+    name_component_t component;
+    name_component_from_string(&component, comp1, sizeof(comp1));
+    printf("***component init***\ncheck type %u\n", (unsigned int) component.type);
+    printf("check length %u\n", component.size);
     printf("check buffer content\n");
     for (size_t i = 0; i < component.size; i++) {
       printf("%d ", component.value[i]);
     }
 
-    // encoding
-    ndn_block_t check_block;
-    size_t block_size = name_component_block_size(&component);
-    uint8_t block_value[block_size];
-    check_block.value = block_value;
-    check_block.size = block_size;
-    name_component_wire_encode(&component, &check_block);
-    printf("\n");
-    printf("check block length %zu\n", check_block.size);
+    // component encoding
+    name_component_block_t check_block;
+    name_component_tlv_encode(&component, &check_block);
+    printf("\n***component encoding***\n");
+    printf("check block length %u\n", check_block.size);
     printf("check block content\n");
     for (size_t i = 0; i < check_block.size; i++) {
       printf("%d ", check_block.value[i]);
     }
 
-    // decoding
-    size_t check_comp_size = decoder_probe_value_size(&check_block);
-    uint8_t check_comp_value[check_comp_size];
+    // component decoding
     name_component_t check_component;
-    check_component.value = check_comp_value;
-    check_component.size = check_comp_size;
     name_component_from_block(&check_component, &check_block);
-    printf("\n");
-    printf("check type %d\n", check_component.type);
-    printf("check length %zu\n", check_component.size);
+    printf("\n***component decoding***\n");
+    printf("check type %u\n", (unsigned int) check_component.type);
+    printf("check length %u\n", check_component.size);
     printf("check buffer content\n");
     for (size_t i = 0; i < check_component.size; i++) {
       printf("%d ", check_component.value[i]);
     }
 
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    // name initialization
+    char comp2[] = "bbbbbb";
+    char comp3[] = "cccccc";
+    char comp4[] = "123456";
+    name_component_t component2;
+    name_component_from_string(&component2, comp2, sizeof(comp2));
+    name_component_t component3;
+    name_component_from_string(&component3, comp3, sizeof(comp3));
+    name_component_t component4;
+    name_component_from_string(&component4, comp4, sizeof(comp4));
+    name_component_t components[3];
+    components[0] = component;
+    components[1] = component2;
+    components[2] = component3;
+
+    ndn_name_t name;
+    ndn_name_init(&name, components, 3);
+    printf("\n***name init***\ncheck name comp size %u\n", name.components_size);
+    for (size_t i = 0; i < name.components_size; i++) {
+      printf("comp type %u\n", (unsigned int) name.components[i].type);
+      for (size_t j = 0; j < name.components[i].size; j++) {
+        printf("%d ", name.components[i].value[j]);
+      }
+      printf("\n");
+    }
+
+    // name append
+    ndn_name_append_component(&name, &component4);
+    printf("***name append comp***\ncheck name comp size %u\n", name.components_size);
+    for (size_t i = 0; i < name.components_size; i++) {
+      printf("comp type %u\n", (unsigned int) name.components[i].type);
+      for (size_t j = 0; j < name.components[i].size; j++) {
+        printf("%d ", name.components[i].value[j]);
+      }
+      printf("\n");
+    }
+
+    // name encode
+    size_t name_block_size = ndn_name_probe_block_size(&name);
+    uint8_t name_block_value[name_block_size];
+    ndn_block_t name_block;
+    name_block.value = name_block_value;
+    name_block.size = name_block_size;
+    ndn_name_tlv_encode(&name, &name_block);
+    printf("\n***name encoding***\n");
+    printf("check block length %zu\n", name_block.size);
+    printf("check block content\n");
+    for (size_t i = 0; i < name_block.size; i++) {
+      printf("%d ", name_block.value[i]);
+    }
+
+    // tests end
+
+    // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
     /* should be never reached */
     return 0;
 }
