8fab3104a03cbf642b062e4be0b087bf2d676af6
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-10-18 14:23:44 -0700

    fix bug in interest unit test

--- unit-test/interest/main.c
+++ unit-test/interest/main.c
@@ -18,66 +18,66 @@
 
 int main(void)
 {
-    /* start shell */
-    puts("All up, running the shell now");
-    // char line_buf[SHELL_DEFAULT_BUFSIZE];
+  /* start shell */
+  puts("All up, running the shell now");
+  // char line_buf[SHELL_DEFAULT_BUFSIZE];
 
-    // tests start
+  // tests start
 
-    // init a name
-    char name_string[] = "/aaa/bbb/ccc/ddd";
-    ndn_name_t name;
-    ndn_name_from_string(&name, name_string, sizeof(name_string));
-    printf("***init a name*** \n");
-    for (size_t i = 0; i < name.components_size; i++) {
-      printf("comp type %u\n", (unsigned int) name.components[i].type);
-      for (size_t j = 0; j < name.components[i].size; j++) {
-        printf("%d ", name.components[i].value[j]);
-      }
-      printf("\n");
-    }
-
-    // init an Interest
-    ndn_interest_t interest;
-    ndn_interest_from_name(&interest, &name);
-    ndn_interest_set_HopLimit(&interest, 1);
-    ndn_interest_set_MustBeFresh(&interest, 1);
-    ndn_interest_set_CanBePrefix(&interest, 1);
-    printf("***init an Interest*** \n");
-    printf("hop limit: %d\n", interest.hop_limit);
-
-    // Interest encodes
-    uint32_t block_size = ndn_interest_probe_block_size(&interest);
-    uint8_t block_value[block_size];
-    uint32_t block_used_size = 0;
-    ndn_interest_encode(&interest, block_value, block_size, &block_used_size);
-    printf("***Interest Encodes*** \n");
-    printf("block size: %d\n", (int) block_size);
-    printf("block content: \n");
-    for (size_t i = 0; i < block_size; i++) {
-      printf("%d ", block_value[i]);
+  // init a name
+  char name_string[] = "/aaa/bbb/ccc/ddd";
+  ndn_name_t name;
+  ndn_name_from_string(&name, name_string, sizeof(name_string));
+  printf("***init a name*** \n");
+  for (size_t i = 0; i < name.components_size; i++) {
+    printf("comp type %u\n", (unsigned int) name.components[i].type);
+    for (size_t j = 0; j < name.components[i].size; j++) {
+      printf("%d ", name.components[i].value[j]);
     }
     printf("\n");
+  }
 
-    // Interest decodes
-    ndn_interest_t check_interest;
-    printf("before function starts\n");
-    int result = ndn_interest_from_block(&check_interest, block_value, block_size);
-    printf("***Interest Decodes*** \n");
-    printf("result number: %d\n", result);
-    printf("hop limit: %d\n", interest.hop_limit);
-    printf("name component size: %d\n", (int) check_interest.name.components_size);
-    for (size_t i = 0; i < check_interest.name.components_size; i++) {
-      printf("comp type %u\n", (unsigned int) check_interest.name.components[i].type);
-      for (size_t j = 0; j < check_interest.name.components[i].size; j++) {
-        printf("%d ", check_interest.name.components[i].value[j]);
-      }
-      printf("\n");
+  // init an Interest
+  ndn_interest_t interest;
+  ndn_interest_from_name(&interest, &name);
+  ndn_interest_set_HopLimit(&interest, 1);
+  ndn_interest_set_MustBeFresh(&interest, 1);
+  ndn_interest_set_CanBePrefix(&interest, 1);
+  printf("***init an Interest*** \n");
+  printf("hop limit: %d\n", interest.hop_limit);
+
+  // Interest encodes
+  uint32_t block_size = ndn_interest_probe_block_size(&interest);
+  uint8_t block_value[block_size];
+  uint32_t block_used_size = 0;
+  ndn_interest_tlv_encode(&interest, block_value, block_size, &block_used_size);
+  printf("***Interest Encodes*** \n");
+  printf("block size: %d\n", (int) block_size);
+  printf("block content: \n");
+  for (size_t i = 0; i < block_size; i++) {
+    printf("%d ", block_value[i]);
+  }
+  printf("\n");
+
+  // Interest decodes
+  ndn_interest_t check_interest;
+  printf("before function starts\n");
+  int result = ndn_interest_from_block(&check_interest, block_value, block_size);
+  printf("***Interest Decodes*** \n");
+  printf("result number: %d\n", result);
+  printf("hop limit: %d\n", interest.hop_limit);
+  printf("name component size: %d\n", (int) check_interest.name.components_size);
+  for (size_t i = 0; i < check_interest.name.components_size; i++) {
+    printf("comp type %u\n", (unsigned int) check_interest.name.components[i].type);
+    for (size_t j = 0; j < check_interest.name.components[i].size; j++) {
+      printf("%d ", check_interest.name.components[i].value[j]);
     }
+    printf("\n");
+  }
 
-    // tests end
+  // tests end
 
-    // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-    /* should be never reached */
-    return 0;
+  // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+  /* should be never reached */
+  return 0;
 }
