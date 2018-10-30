9acfd39ff32a5671de206a481d5d2b9d90275c2b
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-10-30 15:54:09 -0700

    update tests

--- board-test/nrf52840_transmitter/main.c
+++ board-test/nrf52840_transmitter/main.c
@@ -4,13 +4,14 @@
 #include <stdint.h>
 // #include "xtimer.h"
 
-#include "shell.h"
-#include "msg.h"
+// #include "board.h"
+// #include "shell.h"
+// #include "msg.h"
 #include "NRF_802154_driver/src/nrf_802154.h"
 
-static const shell_command_t shell_commands[] = {
-    { NULL, NULL, NULL }
-};
+// static const shell_command_t shell_commands[] = {
+//     { NULL, NULL, NULL }
+// };
 
 #define MAX_MESSAGE_SIZE 16
 #define CHANNEL          11
@@ -18,62 +19,77 @@ static const shell_command_t shell_commands[] = {
 static volatile bool m_tx_in_progress;
 static volatile bool m_tx_done;
 
-int main(int argc, char *argv[])
+#define LED_PORT            (NRF_P0)
+#define LED0_MASK           (1 << 13)
+#define LED1_MASK           (1 << 14)
+#define LED2_MASK           (1 << 15)
+#define LED3_MASK           (1 << 16)
+#define LED_MASK            (LED0_MASK | LED1_MASK | LED2_MASK | LED3_MASK)
+#define LED0_ON             (LED_PORT->OUTCLR = LED0_MASK)
+#define LED0_OFF            (LED_PORT->OUTSET = LED0_MASK)
+#define LED0_TOGGLE         (LED_PORT->OUT   ^= LED0_MASK)
+
+#define LED1_ON             (LED_PORT->OUTCLR = LED1_MASK)
+#define LED1_OFF            (LED_PORT->OUTSET = LED1_MASK)
+#define LED1_TOGGLE         (LED_PORT->OUT   ^= LED1_MASK)
+
+#define LED2_ON             (LED_PORT->OUTCLR = LED2_MASK)
+#define LED2_OFF            (LED_PORT->OUTSET = LED2_MASK)
+#define LED2_TOGGLE         (LED_PORT->OUT   ^= LED2_MASK)
+
+#define LED3_ON             (LED_PORT->OUTCLR = LED3_MASK)
+#define LED3_OFF            (LED_PORT->OUTSET = LED3_MASK)
+#define LED3_TOGGLE         (LED_PORT->OUT   ^= LED3_MASK)
+
+int main(void)
 {
-    (void) argc;
-    (void) argv;
+  printf("802.15.4 packet transmitter starts\n");
 
-    uint8_t message[MAX_MESSAGE_SIZE];
+  uint8_t message[MAX_MESSAGE_SIZE];
 
-    for (uint32_t i = 0; i < sizeof(message) / sizeof(message[0]); i++)
-    {
-        message[i] = i;
+  for (uint32_t i = 0; i < sizeof(message) / sizeof(message[0]); i++) {
+    message[i] = i;
+  }
+
+  message[0] = 0x41;                // Set MAC header: short addresses, no ACK
+  message[1] = 0x98;                // Set MAC header
+
+  m_tx_in_progress = false;
+  m_tx_done        = false;
+
+  nrf_802154_init();
+  LED0_ON;
+  nrf_802154_channel_set(CHANNEL);
+
+  nrf_802154_receive();
+  while (1) {
+    if (m_tx_done) {
+      m_tx_in_progress = false;
+      m_tx_done        = false;
     }
 
-    message[0] = 0x41;                // Set MAC header: short addresses, no ACK
-    message[1] = 0x98;                // Set MAC header
-
-    m_tx_in_progress = false;
-    m_tx_done        = false;
-
-    nrf_802154_init();
-    nrf_802154_channel_set(CHANNEL);
-    nrf_802154_receive();
-
-    while (1)
-    {
-        if (m_tx_done)
-        {
-            m_tx_in_progress = false;
-            m_tx_done        = false;
-        }
-
-        if (!m_tx_in_progress)
-        {
-            m_tx_in_progress = nrf_802154_transmit(message, sizeof(message), true);
-        }
+    if (!m_tx_in_progress) {
+      LED1_ON;
+      m_tx_in_progress = nrf_802154_transmit(message, sizeof(message), true);
     }
+  }
 
-    /* start shell */
-    puts("All up, running the shell now");
-    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-
-    return 0;
+  return 0;
 }
 
 void nrf_802154_transmitted(const uint8_t * p_frame, uint8_t * p_ack,
                             uint8_t length, int8_t power, uint8_t lqi)
 {
-    (void) p_frame;
-    (void) length;
-    (void) power;
-    (void) lqi;
+  (void) p_frame;
+  (void) length;
+  (void) power;
+  (void) lqi;
 
-    m_tx_done = true;
+  LED2_ON;
+  printf("packet successfully transmitted!\n");
+  m_tx_done = true;
 
-    if (p_ack != NULL)
-    {
-        nrf_802154_buffer_free(p_ack);
-    }
+  if (p_ack != NULL) {
+    nrf_802154_buffer_free(p_ack);
+  }
 }
--- unit-test/sign-verify/main.c
+++ unit-test/sign-verify/main.c
@@ -7,80 +7,68 @@
  */
 
 #include <stdio.h>
-
 #include "ndn-riot/security/sign-verify.h"
-#include "shell.h"
-#include "msg.h"
-
-// static const shell_command_t shell_commands[] = {
-//     { NULL, NULL, NULL }
-// };
 
 static uint8_t private[] = {
-    0x00, 0x79, 0xD8, 0x8A, 0x5E, 0x4A, 0xF3, 0x2D,
-    0x36, 0x03, 0x89, 0xC7, 0x92, 0x3B, 0x2E, 0x50,
-    0x7C, 0xF7, 0x6E, 0x60, 0xB0, 0xAF, 0x26, 0xE4,
-    0x42, 0x9D, 0xC8, 0xCE, 0xF0, 0xDE, 0x75, 0xB3
+  0x00, 0x79, 0xD8, 0x8A, 0x5E, 0x4A, 0xF3, 0x2D,
+  0x36, 0x03, 0x89, 0xC7, 0x92, 0x3B, 0x2E, 0x50,
+  0x7C, 0xF7, 0x6E, 0x60, 0xB0, 0xAF, 0x26, 0xE4,
+  0x42, 0x9D, 0xC8, 0xCE, 0xF0, 0xDE, 0x75, 0xB3
 };
 
 static uint8_t public[] = {
-    0xB2, 0xFC, 0x62, 0x14, 0x78, 0xDC, 0x10, 0xEA,
-    0x61, 0x42, 0xB9, 0x34, 0x67, 0xE6, 0xDD, 0xE3,
-    0x3D, 0x35, 0xAA, 0x5B, 0xA4, 0x24, 0x6C, 0xD4,
-    0xB4, 0xED, 0xD8, 0xA4, 0x59, 0xA7, 0x32, 0x12,
-    0x57, 0x37, 0x90, 0x5D, 0xED, 0x37, 0xC8, 0xE8,
-    0x6A, 0x81, 0xE5, 0x8F, 0xBE, 0x6B, 0xD3, 0x27,
-    0x20, 0xBB, 0x16, 0x2A, 0xD3, 0x2F, 0xB5, 0x11,
-    0x1B, 0xD1, 0xAF, 0x76, 0xDB, 0xAD, 0xB8, 0xCE
+  0xB2, 0xFC, 0x62, 0x14, 0x78, 0xDC, 0x10, 0xEA,
+  0x61, 0x42, 0xB9, 0x34, 0x67, 0xE6, 0xDD, 0xE3,
+  0x3D, 0x35, 0xAA, 0x5B, 0xA4, 0x24, 0x6C, 0xD4,
+  0xB4, 0xED, 0xD8, 0xA4, 0x59, 0xA7, 0x32, 0x12,
+  0x57, 0x37, 0x90, 0x5D, 0xED, 0x37, 0xC8, 0xE8,
+  0x6A, 0x81, 0xE5, 0x8F, 0xBE, 0x6B, 0xD3, 0x27,
+  0x20, 0xBB, 0x16, 0x2A, 0xD3, 0x2F, 0xB5, 0x11,
+  0x1B, 0xD1, 0xAF, 0x76, 0xDB, 0xAD, 0xB8, 0xCE
 }; // this is secp160r1 key*/
 
 
 int main(void)
 {
-    /* start shell */
-    puts("All up, running the shell now");
-    // char line_buf[SHELL_DEFAULT_BUFSIZE];
+  /* start shell */
+  puts("All up, running the shell now");
 
-    //initialize
-    uint8_t data[100];
-    uint8_t signature[64];
-    ndn_signer_t signer;
-    ndn_verifier_t verifier;
-    int result = 0;
+  //initialize
+  uint8_t data[100];
+  uint8_t signature[64];
+  ndn_signer_t signer;
+  ndn_verifier_t verifier;
+  int result = 0;
 
-    for (size_t i = 0; i < 100; ++i)
-      data[i] = 0x31;
+  for (size_t i = 0; i < 100; ++i)
+    data[i] = 0x31;
 
-    ndn_signer_init(&signer, data, sizeof(data), signature, sizeof(signature));
-    result = ndn_signer_sha256_sign(&signer);
-    if(result == 0)
-      printf("sha256 signing succeeded\n");
-    ndn_verifier_init(&verifier, data, sizeof(data), signature, signer.output_used_size);
-    result = ndn_verifier_sha256_verify(&verifier);
-    if(result == 0)
-      printf("sha256 verification succeeded\n");
+  ndn_signer_init(&signer, data, sizeof(data), signature, sizeof(signature));
+  result = ndn_signer_sha256_sign(&signer);
+  if(result == 0)
+    printf("sha256 signing succeeded\n");
+  ndn_verifier_init(&verifier, data, sizeof(data), signature, signer.output_used_size);
+  result = ndn_verifier_sha256_verify(&verifier);
+  if(result == 0)
+    printf("sha256 verification succeeded\n");
 
-    ndn_signer_init(&signer, data, sizeof(data), signature, sizeof(signature));
-    result = ndn_signer_ecdsa_sign(&signer, private, 32, NDN_ECDSA_CURVE_SECP160R1);
-    if(result == 0)
-      printf("ecdsa signing succeeded\n");
-    ndn_verifier_init(&verifier, data, sizeof(data), signature, signer.output_used_size);
-    result = ndn_verifier_ecdsa_verify(&verifier, public, 64, NDN_ECDSA_CURVE_SECP160R1);
-    if(result == 0)
-      printf("ecdsa verification succeeded\n");
+  ndn_signer_init(&signer, data, sizeof(data), signature, sizeof(signature));
+  result = ndn_signer_ecdsa_sign(&signer, private, 32, NDN_ECDSA_CURVE_SECP160R1);
+  if(result == 0)
+    printf("ecdsa signing succeeded\n");
+  ndn_verifier_init(&verifier, data, sizeof(data), signature, signer.output_used_size);
+  result = ndn_verifier_ecdsa_verify(&verifier, public, 64, NDN_ECDSA_CURVE_SECP160R1);
+  if(result == 0)
+    printf("ecdsa verification succeeded\n");
 
-    ndn_signer_init(&signer, data, sizeof(data), signature, sizeof(signature));
-    result = ndn_signer_hmac_sign(&signer, private, 32);
-    if(result == 0)
-      printf("hmac signing succeeded\n");
-    ndn_verifier_init(&verifier, data, sizeof(data), signature, signer.output_used_size);
-    result = ndn_verifier_hmac_verify(&verifier, private, 32);
-    if(result == 0)
-      printf("hmac verification succeeded\n");
+  ndn_signer_init(&signer, data, sizeof(data), signature, sizeof(signature));
+  result = ndn_signer_hmac_sign(&signer, private, 32);
+  if(result == 0)
+    printf("hmac signing succeeded\n");
+  ndn_verifier_init(&verifier, data, sizeof(data), signature, signer.output_used_size);
+  result = ndn_verifier_hmac_verify(&verifier, private, 32);
+  if(result == 0)
+    printf("hmac verification succeeded\n");
 
-    // tests end
-
-    // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-    /* should be never reached */
-    return 0;
+  return 0;
 }
