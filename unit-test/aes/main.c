394c4aaa12dfd097d3dce5f84e852d7d5cd050cb
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2018-11-26 22:49:55 -0800

    fix aes unit test

--- unit-test/aes/main.c
+++ unit-test/aes/main.c
@@ -8,79 +8,68 @@
 
 #include <stdio.h>
 
-#include "ndn-riot/security/aes.h"
+#include "ndn_standalone/security/aes.h"
 #include "shell.h"
 #include "msg.h"
 
-static uint8_t iv[16] = {
+static uint8_t iv[] = {
   0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
   0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
 };
 
+static uint8_t key[] = {
+  0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+  0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
+};
+
+static uint8_t data[] = {
+  0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73,
+  0x93, 0x17, 0x2a, 0xae, 0x2d, 0x8a, 0x57, 0x1e, 0x03, 0xac, 0x9c, 0x9e, 0xb7,
+  0x6f, 0xac, 0x45, 0xaf, 0x8e, 0x51, 0x30, 0xc8, 0x1c, 0x46, 0xa3, 0x5c, 0xe4,
+  0x11, 0xe5, 0xfb, 0xc1, 0x19, 0x1a, 0x0a, 0x52, 0xef, 0xf6, 0x9f, 0x24, 0x45,
+  0xdf, 0x4f, 0x9b, 0x17, 0xad, 0x2b, 0x41, 0x7b, 0xe6, 0x6c, 0x37, 0x10
+};
+
 int main(void)
 {
-  puts("All up, running the shell now");
-
   puts("Test AES-CBC Mode");
 
   //initialize
-  uint8_t key[18] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-                     0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 };
-  uint8_t data[16] = { 0x3D, 0x35, 0xAA, 0x5B, 0xA4, 0x24, 0x6C, 0xD4,
-                       0xB4, 0xED, 0xD8, 0xA4, 0x59, 0xA7, 0x32, 0x33 };
   uint32_t j = 0;
-
   printf("print raw data\n");
-  while(j < 16){
+  while (j < sizeof(data)) {
     printf("0x%02x ", data[j++]);
   }
   putchar('\n');
 
+  // encrypt
+  uint8_t cipher_text[sizeof(data) + TC_AES_BLOCK_SIZE] = {0};
   ndn_encrypter_t encrypter;
-  ndn_encrypter_init(&encrypter, key);
-  uint8_t cipher_text_size = sizeof(data) + TC_AES_BLOCK_SIZE;
-  uint8_t cipher_text[cipher_text_size];
-  ndn_encrypter_cbc_set_buffer(&encrypter, data, 16, cipher_text, cipher_text_size);
-  ndn_encrypter_cbc_encrypt(&encrypter, iv);
+  ndn_encrypter_aes_cbc_init(&encrypter, data, sizeof(data),
+                             cipher_text, sizeof(cipher_text));
+  ndn_encrypter_aes_cbc_encrypt(&encrypter, iv, key, sizeof(key));
 
   printf("ciphertext after encryption\n");
   j = 0;
-  while(j < cipher_text_size){
+  while (j < sizeof(data) + TC_AES_BLOCK_SIZE) {
     printf("0x%02x ", cipher_text[j++]);
   }
   putchar('\n');
 
+  // decrypt
+  uint8_t plain_text[sizeof(data)] = {0};
   ndn_decrypter_t decrypter;
-  ndn_decrypter_init(&decrypter, key);
-  uint8_t plain_text[cipher_text_size];
-  ndn_decrypter_cbc_set_buffer(&decrypter, cipher_text, cipher_text_size, plain_text, cipher_text_size);
-  ndn_decrypter_cbc_decrypt(&decrypter, iv);
+  ndn_decrypter_aes_cbc_init(&decrypter, cipher_text, sizeof(cipher_text),
+                             plain_text, sizeof(plain_text));
+  ndn_decrypter_aes_cbc_decrypt(&decrypter, key, sizeof(key));
 
   // print decrypted plain text
   j = 0;
   printf("plaintext after decryption\n");
-  while(j < 16){
+  while(j < sizeof(data)){
     printf("0x%02x ", plain_text[j++]);
   }
 
   // tests end
-
-  puts("\nTest AES");
-  uint8_t plaintext[TC_AES_BLOCK_SIZE];
-  memcpy(plaintext, iv, TC_AES_BLOCK_SIZE);
-  uint8_t expected[TC_AES_BLOCK_SIZE];
-  uint8_t decrypted[TC_AES_BLOCK_SIZE];
-  ndn_encrypter_set_buffer(&encrypter, plaintext, TC_AES_BLOCK_SIZE, expected, sizeof(expected));
-  ndn_encrypter_encrypt(&encrypter);
-  ndn_decrypter_set_buffer(&decrypter, expected, sizeof(expected), decrypted, sizeof(decrypted));
-  ndn_decrypter_decrypt(&decrypter);
-  j = 0;
-  printf("plaintext after decryption\n");
-  while(j < 16){
-    printf("0x%02x ", decrypted[j++]);
-  }
-
-  // shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-  /* should be never reached */
   return 0;
 }
